456f8c08348cab91db72968a761a5fe4b6d37074
Rodaan <rodaan.rabang@gmail.com>
2017-08-19 21:22:22 -0700

    Add some changes

--- endpoints.js
+++ endpoints.js
@@ -16,28 +16,10 @@ function getProblem(req, res){
     console.log(data);
     const jsonData = JSON.parse(data).problems;
     const random = Math.floor(Math.random() * jsonData.length);
-    res.json(jsonData[random]);
+    const selected = jsonData[random];
+    resJson.text = `*${selected.title}*\n${selected.question}`;
+    res.json(resJson);
   });
-  // const resJson = {};
-  // console.log('looking at:');
-  // console.log(file);
-  // nodegit.Repository.open(file)
-  //   .then(repo => repo.getBranchCommit('master'))
-  //   .then((commit) => {
-  //     console.log('got file');
-  //     return commit.getEntry('jeopardy.json');
-  //   })
-  //   .then(entry => entry.getBlob())
-  //   .then((blob) => {
-  //     resJson.questions = JSON.parse(blob.toString()).problems;
-  //     res.json({
-  //       text: resJson.questions[0].question,
-  //     });
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //     res.send(err);
-  //   })
 }
 
 module.exports = {
