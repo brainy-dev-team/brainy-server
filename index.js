0c77a34ae7e2506c53d4fb3e76cd31d4067032b2
Rodaan <rodaan.rabang@gmail.com>
2017-08-19 18:09:08 -0700

    Added post

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const { getProblem } = require('./endpoints');
 
 app.get('/', getProblem);
 
+app.post('/', getproblem);
+
 app.listen(3000, function(){
   console.log('Example app listening on post 3000!');
 });
