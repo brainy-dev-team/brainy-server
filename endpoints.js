f7b650449282aafd60238ed4a7c0cbc5529a42cc
Rodaan <rodaan.rabang@gmail.com>
2017-08-19 20:31:32 -0700

    Fix endpoint

--- endpoints.js
+++ endpoints.js
@@ -6,7 +6,7 @@ function testServer(req, res){
 }
 
 function getProblem(req, res){
-  const file = path.join(__dirname, '/var/app/current/questions');
+  const file = path.join(__dirname, '..', '/var/app/current/questions');
   const resJson = {};
   console.log('looking at:');
   console.log(file);
